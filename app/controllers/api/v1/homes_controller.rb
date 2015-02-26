77fe5a645a3d574ef2288b7b8c7f742483b69906
Reaper <reaper@localhost.localdomain>
2015-02-26 13:49:36 +0530

    added code to homes_controller, created a model called vachana that stores data on database

--- app/controllers/api/v1/homes_controller.rb
+++ app/controllers/api/v1/homes_controller.rb
@@ -1,8 +1,20 @@
 class Api::V1::HomesController < ApplicationController
 
   def today_vachana
-   vachana = "{'vachana': 'ವಚನ'}"
-   render json: vachana , status: :ok
+   @vachana = "{'vachana': 'ವಚನ'}"
+   #render json: vachana , status: :ok
+   #render json: sahitya,vachana , status: :ok
+   render :json => @vachana
+   
  end
-
+ def today_sahitya
+	
+   @sahitya = "{'sahitya': 'ಸಾಹಿತ್ಯ '}"
+   render :json => @sahitya
+ end
+ def today_lister
+	@lister=Vachana.all
+	#@lister=@lister.order(params[:order].gsub(':',' ')) if params[:order]
+	render :json => @lister, :content_type => 'application/json'
+ end
 end
