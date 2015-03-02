0d93f8cd4ad3c590bbab20d24931d85ccdc735c4
devudilip <devu.dilip@gmail.com>
2015-03-03 01:32:48 +0530

    Version updated for ruby

--- app/controllers/api/v1/homes_controller.rb
+++ app/controllers/api/v1/homes_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::HomesController < ApplicationController
 
   def today_vachana
-   vachana = "{'vachana': 'ವಚನ'}"
+   vachana = "{'heading': 'Vachana', 'id': 1, 'vachanakaara': 'Basavanna', 'vachana': 'koodalasangamadeva'}"
    render json: vachana , status: :ok
  end
 
