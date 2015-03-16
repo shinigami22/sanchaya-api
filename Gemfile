da0610206d31b39825b6cd6d39cff7950eca0b53
Reaper <reaper@localhost.localdomain>
2015-03-16 13:00:18 +0530

    Removed pagination code

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ ruby '2.1.5'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use mysql as the database for Active Record
-gem 'will_paginate'
 gem 'mysql2'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- app/controllers/api/v1/homes_controller.rb
+++ app/controllers/api/v1/homes_controller.rb
@@ -1,18 +1,6 @@
 class Api::V1::HomesController < ApplicationController
   def today_vachana
-    @lister=Poet.all
-    #paginate json: @lister
-    @lister=Poet.paginate(:page => params[:page], :per_page => 30)
-    respond_to do | format |
-      format.json{
-        render :json => {
-          :current_page => @lister.current_page,
-          :per_page => @lister.per_page,
-          :total_entries => @lister.total_entries,
-          :total_pages => @lister.total_pages,
-          :entries => @lister
-        }
-      }
-    end
+    @vachana=Poem.all
+    render :json => @vachana
   end
 end
