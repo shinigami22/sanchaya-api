8e07f8041dba5fdce54586af29c74c65e1886f80
shinigami22 <sujith.w22@gmail.com>
2015-04-27 16:32:19 +0530

    Adds show action the user controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@
         get "/search_poem/:search" => "homes#search_poem"
         get "/search_info/:search" => "homes#search_info"
         get "/test_search/:search" => "homes#test_search"
+
       end
     end
+    resources :users, :only => [:show]
     match "*path", to: "errors#catch_404", via: :all
   end
