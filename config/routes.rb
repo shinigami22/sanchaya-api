440c7f05f9252b7ab92fab6ab51b278bdf023298
shinigami22 <sujith.w22@gmail.com>
2015-04-27 21:25:04 +0530

    Updates application controller to prevent CSRF exception from being raised

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@
   Rails.application.routes.draw do
   devise_for :users
     # Api default repond to JSON 
-    namespace :api, defaults: {format: 'json'} do
+    namespace :api, defaults: {format: 'json'} ,
+                                constraints: {subdomain: 'api' }, path: '/' do
       scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
         get "/today_vachana" => "homes#today_vachana"
         get "/list_vachanakaara" => "homes#list_vachanakaara"
