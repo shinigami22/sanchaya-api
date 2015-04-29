9202208424b97e31284dea370bf6cc18f3aa17cc
shinigami22 <sujith.w22@gmail.com>
2015-04-29 14:45:39 +0530

    Added authenticate with method to handle access to actions

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -6,6 +6,7 @@ end
 
 describe Authenticable do
   let(:authentication) { Authentication.new }
+  subject { authentication }
 
   describe "#current_user" do
     before do
@@ -16,5 +17,25 @@ describe Authenticable do
     it "returns the user from the authorization header" do
       expect(authentication.current_user.auth_token).to eql @user.auth_token
     end
+  end
+  describe "#authenticate_with_token" do
+    before do
+      @user = FactoryGirl.create :user
+      #authentication.stub(:current_user).and_return(nil)
+      # response.stub(:response_code).and_return(401)
+      #response.stub(:body).and_return({"errors" => "Not authenticated"}.to_json)
+      #authentication.stub(:response).and_return(response)
+      allow(authentication).to receive(:current_user).and_return(nil)
+      allow(response).to receive(:response_code).and_return(401)
+      allow(response).to receive(:body).and_return({"errors" => "Not authenticated"}.to_json)
+      allow(authentication).to receive(:response).and_return(response)
+
+    end
+
+    it "render a json error message" do
+      json_response = JSON.parse(response.body, symbolize_names: true)
+      expect(json_response[:errors]).to eql "Not authenticated"
+    end
+    it {  should respond_with 401 }
   end
 end
